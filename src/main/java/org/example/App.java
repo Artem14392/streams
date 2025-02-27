a6320239999759e4cf8ef9179e49119e5319475d
Artem14392 <ortexvain3900@mail.ru>
2025-02-28 00:14:07 +0300

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
