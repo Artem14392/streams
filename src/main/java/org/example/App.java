ce684f67980628965c36254e5d3337c1a405d015
Artem14392 <ortexvain3900@mail.ru>
2025-02-28 00:14:36 +0300

    first commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,5 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 /**
  * Hello world!
